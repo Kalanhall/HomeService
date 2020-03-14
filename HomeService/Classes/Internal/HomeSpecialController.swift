55d050490d10bce25ec1d68872acc8d498bfc8ca
Kimee <wujm002@galanz.com>
2020-03-14 11:58:11 +0800

    no message

--- HomeService/Classes/Internal/HomeSpecialController.swift
+++ HomeService/Classes/Internal/HomeSpecialController.swift
@@ -10,29 +10,39 @@ import KLNavigationController
 import Extensions
 import SnapKit
 
-class HomeSpecialController: UIViewController {
+class HomeSpecialController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    lazy var topView: UIImageView = {
+    lazy var topView = { () -> UIImageView in
         let topView = UIImageView()
-        topView.backgroundColor = UIColor.color(hexNumber: 0xFF7D25)
         topView.image = UIImage.image(named: "top", in: Bundle(for: type(of: self)))
         return topView
     }()
     
-    lazy var botView: UIImageView = {
+    lazy var botView = { () -> UIImageView in
         let botView = UIImageView()
-        botView.backgroundColor = UIColor.color(hexNumber: 0xFF7D25)
         botView.image = UIImage.image(named: "bot", in: Bundle(for: type(of: self)))
         return botView
     }()
+    
+    lazy var tableView = { () -> UITableView in
+        let tableView = UITableView(frame: .zero, style: UITableView.Style.plain)
+        tableView.backgroundColor = .clear
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 50
+        tableView.separatorStyle = .none
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.description())
+        return tableView
+    } ()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .white
         kl_barAlpha = 0
         kl_tintColor = .white
         kl_barStyle = .blackOpaque
-        view.backgroundColor = UIColor.color(hexNumber: 0xF5F5F5)
         kl_titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
         navigationItem.title = "每日特价"
         
@@ -42,12 +52,37 @@ class HomeSpecialController: UIViewController {
             make.height.equalTo(102.auto())
         }
         
-        view.addSubview(botView)
+        view.insertSubview(botView, belowSubview: topView)
         botView.snp_makeConstraints { (make) in
             make.leading.trailing.equalToSuperview()
             make.top.equalTo(topView.snp_bottom)
             make.height.equalTo(174.auto())
         }
+        
+        view.addSubview(tableView)
+        tableView.snp_makeConstraints { (make) in
+            make.leading.trailing.bottom.equalToSuperview()
+            make.top.equalTo(topView.snp_bottom)
+        }
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.description()) as! UITableViewCell
+        cell.backgroundColor = .clear
+        
+        return cell
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        var offsetY = scrollView.contentOffset.y
+        if offsetY < 0 {
+            // 下拉
+            offsetY = 0
+        }
+        botView.transform = CGAffineTransform(translationX: 0, y: -offsetY)
+    }
 }
