5c39cf209a58db8c61d19ddbf7ea1c67015a9bce
692015 <wujm002@galanz.com>
2020-08-26 18:56:48 +0800

    no message

--- HomeService/Classes/View/HomeGraphicCell.swift
+++ HomeService/Classes/View/HomeGraphicCell.swift
@@ -14,12 +14,14 @@ class HomeGraphicCell: ASCellNode {
     let iconNode = ASNetworkImageNode()
     let nameNode = ASTextNode()
     let timeNode = ASTextNode()
+    let textNode = ASTextNode()
+    var imagesNode: ASCollectionNode!
     let lineNode = ASTextNode()
     
     override init() {
         super.init()
         
-        iconNode.style.preferredSize = CGSize(width: 50.auto(), height: 50.auto())
+        iconNode.style.preferredSize = CGSize(width: 40.auto(), height: 40.auto())
         iconNode.image = UIImage.image(named: "logo", in: Bundle(for: HomeGraphicCell.self))
         addSubnode(iconNode)
         
@@ -34,8 +36,22 @@ class HomeGraphicCell: ASCellNode {
                                                                   .foregroundColor : UIColor.color(hexNumber: 0xE5E5E5)])
         addSubnode(timeNode)
         
-//        lineNode.backgroundColor = UIColor.color(hexNumber: 0xE3E3E3)
-//        addSubnode(lineNode)
+        textNode.maximumNumberOfLines = 5
+        textNode.attributedText = NSAttributedString(string: "时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切时间会说明一切",
+                                                     attributes: [.font : UIFont.systemFont(ofSize: 13),
+                                                                  .foregroundColor : UIColor.color(hexNumber: 0x222222)])
+        addSubnode(textNode)
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        imagesNode = ASCollectionNode(collectionViewLayout: layout)
+        imagesNode.style.preferredSize = CGSize(width: 0, height: 150.auto())
+        addSubnode(imagesNode)
+        
+        lineNode.backgroundColor = UIColor.color(hexNumber: 0xE3E3E3)
+        lineNode.style.flexGrow = 1
+        lineNode.style.preferredSize = CGSize(width: 0, height: 0.5)
+        addSubnode(lineNode)
         
         let backView = UIView()
         backView.backgroundColor = UIColor.color(hexNumber: 0xF9F9F9)
@@ -44,18 +60,27 @@ class HomeGraphicCell: ASCellNode {
 
     // ASLayoutSpec
     override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
-        // 局部
+        
+        // 头部 - 文字
         let name = ASStackLayoutSpec.vertical()
         name.style.flexShrink = 1
         name.style.flexGrow = 1
         name.spacing = 10
         name.children = [nameNode, timeNode]
-        // 整体
-        let conten = ASStackLayoutSpec(direction: .horizontal,
+        
+        // 头部
+        let header = ASStackLayoutSpec(direction: .horizontal,
                                        spacing: 10,
                                        justifyContent: .start,
-                                       alignItems: .start,
+                                       alignItems: .center,
                                        children: [iconNode, name])
-        return ASInsetLayoutSpec(insets: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10), child: conten)
+        
+        // 整体
+        let conten = ASStackLayoutSpec.vertical()
+        conten.style.flexShrink = 1
+        conten.style.flexGrow = 1
+        conten.spacing = 10
+        conten.children = [header, textNode, imagesNode, lineNode]
+        return ASInsetLayoutSpec(insets: UIEdgeInsets(top: 10, left: 10, bottom: 0, right: 10), child: conten)
     }
 }
